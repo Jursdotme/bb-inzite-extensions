57b8ad20f7e078188fa94451b67974cee190648d
Rasmus Jürs <rasmus@jurs.me>
2017-10-31 09:38:33 +0100

    Version update

--- bb-inzite-extensions.php
+++ bb-inzite-extensions.php
@@ -1,26 +1,46 @@
 <?php
 /**
- * Plugin Name: Inzite Custom Modules
+ * Plugin Name: Inzite Beaver Builder Extensions
  * Plugin URI: http://www.inzite.dk
- * Description: Custom modules for the Beaver Builder Plugin.
- * Version: 1.0
+ * Description: Custom modules and extensions for the Beaver Builder.
+ * Version: 0.0.1
  * Author: Rasmus Jürs
  * Author URI: http://www.jurs.me
+ */
+
+/**
+ * Add update check
+ */
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/user-name/repo-name/',
+	__FILE__,
+	'unique-plugin-or-theme-slug'
+);
+
+/**
+ * Create relative constants
  */
 define( 'INZ_BB_E_DIR', plugin_dir_path( __FILE__ ) );
 define( 'INZ_BB_E_URL', plugins_url( '/', __FILE__ ) );
 
-// Load custom modules
+
+/**
+ * Load custom modules
+ */
 function inz_load_modules() {
     if ( class_exists( 'FLBuilder' ) ) {
         // Include your custom modules here.
         require_once 'inz-simple-slider-module/inz-simple-slider-module.php';
+        require_once 'inz-definition-list-module/inz-definition-list-module.php';
         //require_once 'inz-divider-module/inz-divider-module.php';
     }
 }
-// add_action( 'init', 'inz_load_modules' );
+add_action( 'init', 'inz_load_modules' );
 
-// Load custom functions
+/**
+ * Load custom functions
+ */
 require_once 'inc/extended-row-margins.php';
 require_once 'inc/sticky-columns.php';
 require_once 'inc/column-box-shadows.php';
