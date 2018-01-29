156df4d706e2edea46611972a624f0544609372d
Rasmus Jürs <rasmus@jurs.me>
2018-01-29 10:54:13 +0100

    fix link

--- modules/inz-feature-module/includes/frontend.php
+++ modules/inz-feature-module/includes/frontend.php
@@ -1,5 +1,5 @@
 
-<a href="#" class="bjerg" style="background-image: url(<?php echo $settings->background_image_src; ?>)">
+<a href="<?php echo $settings->link; ?>" class="bjerg" style="background-image: url(<?php echo $settings->background_image_src; ?>)">
 	<div class="bjerg__caption">
 		<h2><?php echo $settings->heading; ?></h2>
 		<p><?php echo $settings->sub_heading; ?></p>
